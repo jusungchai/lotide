05ff7d0bfac5883a353126489175dff9311a4d6b
Jay Chai <chunsaa@gmail.com>
2019-10-15 16:05:46 +0000

    Initial Commit

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u{1f605} \u{1f605} \u{1f605} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (str){
+  let results = {};
+  for (let char of str){
+    if (char !== " "){
+      if (results[char]){
+        results[char]++;
+      }
+      else results[char] = 1;
+    }    
+  }
+  return results;
+};
+
+console.log(countLetters("yolo"));
+console.log(countLetters("lighthouse in the house"));
+console.log(countLetters("  lighthouse in the hou   se  "));
+console.log(countLetters(" "));
