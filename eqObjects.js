16b4442c892044bcfe9286d58f12a867180bbf3f
Jay Chai <chunsaa@gmail.com>
2019-10-16 20:27:51 +0000

    Changed to recursion and added nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -10,7 +10,7 @@ const assertEqual = function (actual, expected) {
 const eqArrays = function (a1, a2) {
   if (a1.length !== a2.length) return false;
   else {
-    for (let i = 0; i < a1.length; i++){
+    for (let i = 0; i < a1.length; i++) {
       if (a1[i] !== a2[i]) return false;
     }
     return true;
@@ -19,19 +19,30 @@ const eqArrays = function (a1, a2) {
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function(object1, object2) {
+const eqObjects = function (object1, object2) {
   let keyArr1 = Object.keys(object1);
   let keyArr2 = Object.keys(object2);
   if (keyArr1.length != keyArr2.length) return false;
-  for (key of keyArr1){    
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])){
+  for (key of keyArr1) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) return false;
       else continue;
+    }
+    if (!Array.isArray(object1[key]) && typeof (object1[key]) === "object" && !Array.isArray(object2[key]) && typeof (object2[key]) === "object") {
+      return eqObjects(object1[key], object2[key]);
     }
     if (object1[key] !== object2[key]) return false;
   }
   return true;
 };
+
+
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })); // => false
+console.log(eqObjects({a:{}},{a:{}}));
+
+
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
@@ -46,3 +57,5 @@ assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
+
+
