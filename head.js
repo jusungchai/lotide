8862a3962704320e50f6eab9784f2f10fe09fe77
Jay Chai <chunsaa@gmail.com>
2019-10-21 02:45:56 +0000

    added require and export for testing

--- head.js
+++ head.js
@@ -1,20 +1,24 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
+/* const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`\u{1f605} \u{1f605} \u{1f605} Assertion Failed: ${actual} !== ${expected}`);
   }
-};
+}; */
 
-function head(arr){
-  if (arr.length > 0){
+const assertEqual = require('./assertEqual');
+
+const head = function (arr) {
+  if (arr.length > 0) {
     return arr[0];
   } else {
     return undefined;
   }
 }
 
-assertEqual(head([5,6,7]), 5);
+module.exports = head;
+
+/* assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 5);
+assertEqual(head([]), 5); */
