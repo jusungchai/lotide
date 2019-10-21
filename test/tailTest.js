fd6ddaf366e53784105d99d800a6a145a9cddeae
Jay Chai <chunsaa@gmail.com>
2019-10-21 03:54:04 +0000

    added chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,19 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+//const assertEqual = require('../assertEqual');
 const tail = require('../tail');
+const result = tail(["Hello", "Lighthouse", "Labs"]);
 
-// Test Case 1: Check the returned array elements
+describe("#tail", () => {
+  it('should return "Lighthouse" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(result[0], "Lighthouse");
+  });
+
+  it('should return "Labs" for ["Hello", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(result[1], "Labs");
+  });
+});
+
+/* // Test Case 1: Check the returned array elements
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
@@ -10,4 +22,4 @@ assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(words.length, 3); // original array should still have 3 elements! */
