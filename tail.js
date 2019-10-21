949ab33bd9bbdae0df7063be31ce9b3009e016b4
Jay Chai <chunsaa@gmail.com>
2019-10-21 02:50:37 +0000

    added export for testing

--- tail.js
+++ tail.js
@@ -1,11 +1,11 @@
-// FUNCTION IMPLEMENTATION
+/* // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`\u{1f605} \u{1f605} \u{1f605} Assertion Failed: ${actual} !== ${expected}`);
   }
-};
+}; */
 
 let tail = function(arr) {
   if (arr.length <= 1) {
@@ -16,7 +16,9 @@ let tail = function(arr) {
   }
 };
 
-// Test Case 1: Check the returned array elements
+module.exports = tail;
+
+/* // Test Case 1: Check the returned array elements
 const result = tail(["Hello", "Lighthouse", "Labs"]);
 assertEqual(result.length, 2); // ensure we get back two elements
 assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
@@ -25,5 +27,5 @@ assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(words.length, 3); // original array should still have 3 elements! */
 
